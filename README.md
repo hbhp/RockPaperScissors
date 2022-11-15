98c4a3c2be6c71e2e43089f839d6adb9cdc75dfa
Hallam Breen <hal.breen95@gmail.com>
2022-11-15 21:18:19 +0000

    Write pseudocode in java.js

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # RockPaperScissors
+A basic Javascript interface, replicating a game of Rock, Paper, Scissors between the user and the computer, playable through the console
---
+++ java.js
@@ -0,0 +1,15 @@
+function getComputerChoice {
+    //set 'rock' as variable (not case dependent)
+    //set 'paper' as variable (not case dependent)
+    //set 'scissors' as variable (not case dependent)
+    //computer selects one variable
+    //result = Does this need to be a different value?
+}
+
+//create interface for user choice = playerSelection
+
+function declaration {
+    //take playerSelection
+    //take getComputerChoice (or alternative result variable)
+    //if/else
+}
