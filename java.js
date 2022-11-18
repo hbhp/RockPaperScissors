9708d99223ec9fb96da69c7c4852190d96fd819a
Hallam Breen <hal.breen95@gmail.com>
2022-11-18 10:02:44 +0000

    Explore consturction of computer choice function

--- java.js
+++ java.js
@@ -1,14 +1,16 @@
-function getComputerChoice {
-    //set 'rock' as variable (not case dependent)
-    //set 'paper' as variable (not case dependent)
-    //set 'scissors' as variable (not case dependent)
-    //computer selects one variable
-    //result = Does this need to be a different value?
+function getComputerChoice(){
+    let compArray = ('Rock','Paper','Scissors');
+    let randomChoice = Math.floor(Math.random()*compArray.length);
+    return randomChoice;
 }
 
+//let getComputerChoice = () => {  
+//    let result = 'Rock' || 'Paper' || 'Scissors';
+//    return result;
+//}
 //create interface for user choice = playerSelection
 
-function declaration {
+function declaration (){
     //take playerSelection
     //take getComputerChoice (or alternative result variable)
     //if/else
