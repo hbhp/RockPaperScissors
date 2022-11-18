3c846e515adb12d321741c67a50046fecc7df382
Hallam Breen <hal.breen95@gmail.com>
2022-11-18 12:51:21 +0000

    Build playRound function

--- java.js
+++ java.js
@@ -1,19 +1,38 @@
 function getComputerChoice(){
-    let num1 = Math.random();
-    if (num1 <= 0.333){
+    let computerSelection = Math.random();
+    if (computerSelection <= 0.333){
         return 'ROCK';
-    } else if (num1 >0.333 && num1 <=0.666) {
+    } else if (computerSelection >0.333 && computerSelection <=0.666) {
         return 'PAPER';
-    } else if (num1 > 0.666) {
+    } else if (computerSelection > 0.666) {
         return 'SCISSORS';
     }
 }
 
-//let getComputerChoice = () => {  
-//    let result = 'Rock' || 'Paper' || 'Scissors';
-//    return result;
-//}
-//create interface for user choice = playerSelection
+function playerSelection(){
+    const ask = prompt ('Rock, Paper or Scissors?').toUpperCase;
+    return ask;
+}
+
+function playRound (computerSelection, playerSelection){
+    if (computerSelection === playerSelection){
+        return 'It\'s a tie! Try again!';
+    } else if (computerSelection === 'PAPER' && playerSelection === 'ROCK'){
+        return 'Computer wins! Paper beats rock!';
+    } else if (computerSelection === 'PAPER' && playerSelection === 'SCISSORS'){
+        return 'You win! Scissors beats paper!';
+    } else if (computerSelection === 'ROCK' && playerSelection === 'PAPER'){
+        return 'You win Paper beats rock!';
+    } else if (computerSelection === 'ROCK' && playerSelection === 'SCISSORS'){
+        return 'Computer wins! Rock beats scissors!';
+    } else if (computerSelection === 'SCISSORS' && playerSelection === 'ROCK'){
+        return 'You win! Rock beats scissors!';
+    } else if (computerSelection === 'SCISSORS' && playerSelection === 'PAPER'){
+        return 'Computer wins! Scissors beats paper!';
+    } else () => {
+        return 'Something when wrong, let\'s try again!';
+    }
+}
 
 function declaration (){
     //take playerSelection
