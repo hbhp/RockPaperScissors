0b39b485dbca6806fe6250e9758c76392ae511f8
Hallam Breen <hal.breen95@gmail.com>
2022-11-18 18:18:39 +0000

    Proof getComputerChoice and playRound functions

--- java.js
+++ java.js
@@ -8,24 +8,23 @@ function getComputerChoice(){
         return 'SCISSORS';
     }
 }
-let playerSelection = playerInput.toUpperCase();
 
 function playRound (computerSelection, playerSelection){
-    if (computerSelection === playerSelection){
+    if (computerSelection === playerSelection.toUpperCase()){
         return 'A tie!';
-    } else if (computerSelection === 'PAPER' && playerSelection === 'ROCK'){
+    } else if (computerSelection === 'PAPER' && playerSelection.toUpperCase() === 'ROCK'){
         return 'Computer wins! Paper beats rock!';
-    } else if (computerSelection === 'PAPER' && playerSelection === 'SCISSORS'){
+    } else if (computerSelection === 'PAPER' && playerSelection.toUpperCase() === 'SCISSORS'){
         return 'You win! Scissors beats paper!';
-    } else if (computerSelection === 'ROCK' && playerSelection === 'PAPER'){
+    } else if (computerSelection === 'ROCK' && playerSelection.toUpperCase() === 'PAPER'){
         return 'You win Paper beats rock!';
-    } else if (computerSelection === 'ROCK' && playerSelection === 'SCISSORS'){
+    } else if (computerSelection === 'ROCK' && playerSelection.toUpperCase() === 'SCISSORS'){
         return 'Computer wins! Rock beats scissors!';
-    } else if (computerSelection === 'SCISSORS' && playerSelection === 'ROCK'){
+    } else if (computerSelection === 'SCISSORS' && playerSelection.toUpperCase() === 'ROCK'){
         return 'You win! Rock beats scissors!';
-    } else if (computerSelection === 'SCISSORS' && playerSelection === 'PAPER'){
+    } else if (computerSelection === 'SCISSORS' && playerSelection.toUpperCase() === 'PAPER'){
         return 'Computer wins! Scissors beats paper!';
-    } else () => {
+    } else {
         return 'Something when wrong, let\'s try again!';
     }
 }
